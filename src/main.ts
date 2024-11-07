5a4c75aaa39c07a78fd979d78a702e70fa9a08b5
Rihan Arfan <me@file.properties>
2024-11-07 05:07:27 +0000

    feat: create github action

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import * as glob from '@actions/glob'
+import * as httpClient from '@actions/http-client'
+import * as fs from 'fs/promises'
 
 /**
  * The main function for the action.
@@ -7,20 +9,117 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const { default: mime } = await import('mime')
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const projectKey = core.getInput('project-key')
+    const directory = core.getInput('directory')
+    const hubUrl = core.getInput('hub-url')
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    if (projectKey) core.debug(`Linked with: ${projectKey}`)
+    core.debug(`Using Nuxt output directory: ${directory}`)
+    core.debug(`Using Hub URL: ${hubUrl}`)
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    // Get CICD token
+    const audience = projectKey
+      ? new URL(`${hubUrl}/projects/${projectKey}`).toString()
+      : undefined
+    const idToken = await core.getIDToken(audience)
+
+    // Get project info
+    const http = new httpClient.HttpClient('nuxt-hub-action')
+    const projectInfoResponse = await http.getJson<{
+      accessToken: string
+      teamSlug: string
+      projectSlug: string
+    }>(`${hubUrl}/api/cicd/token`, {
+      authorization: `Bearer ${idToken}`
+    })
+    if (!projectInfoResponse.result) {
+      throw new Error('Project not found')
+    }
+    const projectInfo = projectInfoResponse.result!
+
+    core.setSecret(projectInfo.accessToken)
+
+    // Validate directory
+    try {
+      await fs.access(directory)
+    } catch {
+      throw new Error(`Directory ${directory} does not exist`)
+    }
+
+    // Glob files
+    const globber = await glob.create(`${directory}/**/*`, {
+      implicitDescendants: false
+    })
+    const files = await globber.glob()
+
+    // Process files for deployment
+    const fileKeys = files.map(file => {
+      return file.substring(directory.length + 1)
+    })
+
+    // Filter files to deploy
+    const filesToDeploy = fileKeys.filter(fileKey => {
+      if (fileKey.startsWith('.wrangler/')) return false
+      if (fileKey.startsWith('node_modules/')) return false
+      if (fileKey.startsWith('database/migrations/')) return false
+      if (fileKey === 'wrangler.toml') return false
+      if (fileKey === '.dev.vars') return false
+      return true
+    })
+
+    // Validate hub.config.json existence
+    if (!filesToDeploy.includes('hub.config.json')) {
+      throw new Error(
+        `${directory}/hub.config.json is missing, please make sure that @nuxthub/core is enabled in your nuxt.config.ts.`
+      )
+    }
+
+    // Create deployment file array
+    const deployFiles = await Promise.all(
+      filesToDeploy.map(async fileKey => {
+        const filepath = `${directory}/${fileKey}`
+        const content = await fs.readFile(filepath)
+        return {
+          path: fileKey,
+          key: Buffer.from(filepath).toString('base64'),
+          value: content.toString('base64'),
+          base64: true,
+          metadata: {
+            contentType: mime.getType(filepath) || 'application/octet-stream'
+          }
+        }
+      })
+    )
+
+    const deployment = await http.postJson<{
+      url: string
+      primaryUrl: string
+    }>(
+      `${hubUrl}/teams/${projectInfo.teamSlug}/projects/${projectInfo.projectSlug}/deploy`,
+      { files: deployFiles },
+      { authorization: `Bearer ${projectInfo.accessToken}` }
+    )
+
+    if (!deployment.result || deployment.statusCode !== 200) {
+      const result = deployment.result as any
+
+      if (result?.data?.name === 'ZodError') {
+        throw new Error(JSON.stringify(result.data.issues))
+      } else if (result?.message?.includes('Error: ')) {
+        throw new Error(result.message.split('Error: ')[1])
+      } else if (result?.message) {
+        throw new Error(result.message.split(' - ')[1] || result.message)
+      }
+
+      throw new Error('Deployment failed')
+    }
+
+    // Set outputs
+    core.setOutput('deployment-url', deployment.result.url)
+    core.setOutput('primary-deployment-url', deployment.result.primaryUrl)
   } catch (error) {
-    // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
   }
 }
